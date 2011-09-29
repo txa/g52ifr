bdeac8f8b25bc4d78f76045488cfe855f45800d2
Thorsten Altenkirch <txa@cs.nott.ac.uk>
2011-09-30 00:23:36 +0100

    more on prop

--- lectures/src/Prop.v
+++ lectures/src/Prop.v
@@ -1,3 +1,4 @@
+
 (* Copyright (c) 2011, Thorsten Altenkirch *)
 
 (** %\chapter{Propositional Logic}% *)
@@ -24,7 +25,7 @@ like "The sun is shining" or "We go to the zoo." *)
 - Conjunction [/\], read [P /\ Q] as [P] and [Q].
 - Disjunction [\/], read [P \/ Q] as [P] or [Q].
 - [False], read [False] as "Pigs can fly".
-- [True], read [True] as "This is obvious."
+- [True], read [True] as "It sometimes rains in England."
 - Negation [~], read [~ P] as not [P]. We define [~ P] as [P -> False].
 - Equivalence, [<->], read [P <-> Q] as [P] is equivalent to [Q]. We define [P <-> Q] as [(P -> Q) /\ (Q -> P)].
 
@@ -37,13 +38,13 @@ As in algebra we use parentheses to group logical expressions. To save parenthes
 This is not a complete specification. If in doubt use parentheses.
 *)
 
-(** We will now discuss how to prove something in Coq. If we are
+(** We will now discuss how to prove propositions in Coq. If we are
 proving a statement containing propositional variables then this means
 that the statement is true for all replacements of the variables with
 actual propositions. We say it is a tautology. 
 *)
 
-(** * Implication *)
+(** * Our first proof *)
 
 (** We start with a very simple tautology [P -> P], i.e. if [P] then [P]. 
 To start a proof we write:*)
@@ -75,11 +76,256 @@ Qed.
 
 (** Qed stands for "Quod erat demonstrandum". This is Latin for "What was to be shown." *)
 
+(** * Using assumptions. *)
 
 (** Next we will prove another tautology, namely 
 [(P -> Q) -> (Q -> R) -> P -> R]
+Try to understand why this is intuitively true for any propositions [P],[Q] and [R]
+
+To prove this in Coq we need to know how to use an implication which we have assumed.
+This can be done using the [apply] tactic.
 *)
 
+Lemma C : (P -> Q) -> (Q -> R) -> P -> R.
+
+(** We have to prove an implication, hence we will be using [intro].
+Because [->] is right associative the proposition can be written as
+[(P -> Q) -> ((Q -> R) -> P -> R)]. Hence we are going to assume [P -> Q].
+*)
+
+intro pq. 
+
+(** we continue assuming... *)
+
+intro qr.
+intro p.
+
+(** Now we have three assumptions [P -> Q], [Q -> R] and [P]. 
+It remains to prove [R]. We cannot use [intro] anymore because our goal is not 
+an implication. Instead we need to use our assumptions. The only assumption which 
+could help us to prove [R] is [Q -> R]. We use the [apply] tactic. *)
+
+apply qr.
+
+(** Apply uses [Q -> R] to reduce the problem to prove [R] to the problem to prove [Q].
+Which in turn can be further reduced to proving [P] using [P -> Q].
+*)
+
+apply pq.
+
+(** And now it only remains to prove [P] which is one of our assumptions - hence we can use [exact] again. 
+*)
+
+exact p.
+Qed.
+
+(** * Introduction and Elimination *)
+
+(** We observe that there are two types of proof steps (tactics):
+- introduction: How can we prove a proposition? In the case of an implication this is [intro]. To prove [P -> Q], we assume [P] and prove [Q].
+- elimination: How can we use an assumption? In the case of implication this is [apply]. If we know [P -> Q] and we want to prove [Q] it is sufficent to prove [P].
+Actually [apply] is a bit more general: if we know [P1 -> P2 -> ... -> Pn -> Q] and we want to prove [Q] then it is sufficent to prove [P1],[P2],...,[Pn].
+
+We will observe that the distinction of introduction and elimination
+steps is applicable to all the connectives we are going to
+encounter. This is a fundamental symmetry in reasoning.
+*)
+
+(** There is also a 3rd kind of steps: structural steps.
+    An example is [exact] which we can use when we want to refer to an assumption.
+    We can also use [assumption] then we don't even have to give the name of the assumption. *)
+
+(** If we want to do several [intro] steps we can use [intros]. *)
+
+(** * Conjunction *)
+
+(** How to prove a conjunction? To prove [P /\ Q] we need to prove [P] and [Q]. This is achieved using the [split] tactic. We look at a simple example. *)
+
+Lemma pair : P -> Q -> P /\ Q.
+
+(** On the top level we have to prove an implication. *)
+
+intros p q.
+
+(** now to prove [P /\ Q] we use [split]. *)
+
+split.
+
+(** This creates two subgoals. We do the first *)
+
+exact p.
+
+(** And then the 2nd *)
+
+exact q.
+Qed.
 
 
+(** How do we use and assumption [P /\ Q]. We use [destruct] to split it into two assumptions. 
+    As an example we prove that [P /\ Q -> Q /\ P].
+*)
 
+Lemma andCom : P /\ Q -> Q /\ P.
+intro pq.
+split.
+
+(** Now we need to use the assumption [P /\ Q]. We destruct it into two assumptions: [P] and [Q].
+[destruct] allows us to name the new assumptions. *)
+
+destruct pq as [p q].
+exact q.
+destruct pq as [p q].
+exact p.
+Qed.
+
+(** Can you see a shorter proof of the same theorem ? *)
+
+(** To summarize for conjunction we have:
+- introduction: [split]: to prove [P /\ Q] we prove both [P] and [Q].
+- elimination: [destruct]: to prove something from [P /\ Q] we prove it from assuming both [P] and [Q].
+*)
+
+(** * The currying theorem *)
+
+(** Maybe you have already noticed that a statement like [P -> Q -> R] basically means that [R] can be proved from assuming both [P] and [Q]. Indeed, it is equivalent to [P /\ Q -> R]. We can show this formally and at the same time for the first time use [<->].
+
+All the steps we have already explained so I won't comment. It is a good idea to step through the proof using coq.
+*)
+
+Lemma curry : (P /\ Q -> R) <-> (P -> Q -> R).
+split.
+intros H p q.
+apply H.
+split.
+exact p.
+exact q.
+intros pqr pq.
+apply pqr.
+destruct pq as [p q].
+exact p.
+destruct pq as [p q].
+exact q.
+Qed.
+
+(** I call this the currying theorem, because this is the logical counterpart of currying in functional programming: i.e. that a function with several parameters can be reudced to a function which returns a function. So in Haskell addition has the type [Int -> Int -> Int]. *)
+
+(** * Disjunction *)
+
+(** To prove a disjunction like [P \/ Q] we can either prove [P] or [Q]. This is done via the tactics [left] and [right]. As an example we prove [P -> P \/ Q]. *)
+
+Lemma inl : P -> P \/ Q.
+intros p.
+
+(** Clearly, here we have to use [left]. *)
+
+left.
+exact p.
+Qed.
+
+(** To use a disjunction [P \/ Q] to prove something we have to prove it from both [P] and [Q]. The tactic we use is also called [destruct] but in this case [destruct] creates two subgoals. This can be compared to case analysis in functional programming. Indeed we can prove the following theorem. *)
+
+Lemma case : P \/ Q -> (P -> R) -> (Q -> R) -> R.
+intros pq pr qr.
+destruct pq as [p | q].
+
+(** The syntax for [destruct] for disjunction is different if we want to name the assumption we have to separate them with [|]. Indeed each of them will be visible in a different part of the proof. 
+First we assume [P].*)
+
+apply pr.
+exact p.
+
+(** And then we assume [Q] *)
+
+apply qr.
+exact q.
+Qed.
+
+(** So again to summarize: For disjunction we have:
+- introduction: there are two ways to prove a disjunction [P \/ Q]. We use [left] to prove it from [P] and [right] to prove it from [Q]. 
+- elimination: If we have assumed [P \/ Q] then we can use [destruct] to prove our current goal from assuming [P] and from assuming [Q].
+*)
+
+(** * Distributivity *)
+
+(** As an example of how to combine the proof steps for conjunction
+and disjunction we show that distributivity holds, i.e. [P /\ (Q \/
+R)] is logically equivalent to [(P /\ Q) \/ (P /\ R)]. This is
+reminiscient of the principle in algebra that [x*(y + z) = x*y + x*z]. 
+*)
+
+Lemma andOrDistr : P /\ (Q \/ R) 
+              <-> (P /\ Q) \/ (P /\ R).
+split.
+intro pqr.
+destruct pqr as [p qr].
+destruct qr as [q | r].
+left.
+split.
+exact p.
+exact q.
+right.
+split.
+exact p.
+exact r.
+intro pqpr.
+destruct pqpr as [pq | pr].
+split.
+destruct pq as [p q].
+exact p.
+left.
+destruct pq as [p q].
+exact q.
+destruct pr as [p r].
+split.
+exact p.
+right.
+exact r.
+Qed.
+
+(** As before: to understand the working of this script it is advisible to step through it using Coq. *)
+
+(** * True and False *)
+
+(** [True] is just a conjunction with no arguments as opposed to [/\] which has two. Similarily [False] is a dijsunction with no arguments. As a consequence we already know the proof rules for [True] and [False].
+
+We can prove [True] without any assumptions.
+*)
+
+Lemma triv : True.
+split.
+
+(** Here we split but instead of two subgoals we get none. *)
+
+Qed.
+
+(** On the other had we can prove anything from [False]. This is called "ex falso quod libet" in latin.
+*)
+
+Lemma exFalso : False -> P.
+intro f.
+destruct f.
+
+(** Here instead of two subgoals we get none. *)
+
+Qed.
+
+(** * Negation *)
+
+(** [~ P] is defined as [P -> False]. Using this we can establish some basic theorems about negation. First we show that we connot have both [P] and [~P], that is we prove [~ (P /\ ~P)].
+*)
+
+Lemma incons : ~( P /\ ~P).
+intro h.
+destruct h as [p np].
+apply np.
+exact p.
+Qed.
+
+
+(** Another example is to show that [P] implies [~~P]. *)
+
+Lemma p2nnp : P -> ~~P.
+intros p np.
+apply np.
+exact p.
+Qed.
