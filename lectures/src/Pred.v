83811b69f9d2a4e31faea63966831e37fe229161
Thorsten Altenkirch <txa@wererabbit.cs.nott.ac.uk>
2011-10-14 16:20:56 +0100

    exists...

--- lectures/src/Pred.v
+++ lectures/src/Pred.v
@@ -104,26 +104,99 @@ if [P] is [Q x /\ forall x:A,R x x] then the only free occurence of [x] is the o
 We can also use [intros] here. That is if the current goal is [forall x:A,P x -> Q x] then 
 [intros x P] will introduce the assumptions [x:A] and [H:P x].
 
-The general case for [apply] is a bit hard to explain. Basically apply may introduce several
+The general case for [apply] is a bit hard to describe. Basically apply may introduce several
 subgoals if the assumption has a prefix of [forall] and [->]. E.g. if we have assumed
 [H : forall x:A,forall y:B,P x -> Q y -> R x y] and our current goal is [R a b] then
-[apply H] will instantiate [x] with [a] and [y] with [b] and generate the new goals [
+[apply H] will instantiate [x] with [a] and [y] with [b] and generate the new goals 
+[Q b] and [R a b].
 
-
-    
-
-
-
-(** Next we are going to show that [forall] _commutes with_ [/\]. That is
-    we are going to show [(forall x:A,P x /\ Q x) <-> (forall x:A, P x) /\ (forall x:A, Q x)]
-    that is "all students are clever and funny" is equivalent to "all students are clever"
-    and "all studnets are funny". *)
+Next we are going to show that [forall] _commutes with_ [/\]. That is
+we are going to show [(forall x:A,P x /\ Q x) <-> (forall x:A, P x) /\ (forall x:A, Q x)]
+that is "all students are clever and funny" is equivalent to "all students are clever"
+and "all students are funny". 
+*)
 
 Lemma AllAndCom : (forall x:A,P x /\ Q x) <-> (forall x:A, P x) /\ (forall x:A, Q x).
 split.
 (** Proving [->] *)
 intro H.
 split.
+intro a.
+assert (pq : P a /\ Q a).
+apply H.
+destruct pq as [p q].
+exact p.
+intro a.
+assert (pq : P a /\ Q a).
+apply H.
+destruct pq as [p q].
+exact q.
+(** Proving [<-] *)
+intro H.
+destruct H as [p q].
+intro a.
+split.
+apply p.
+apply q.
+Qed.
+
+(** This proof is quite lengthy and I even had to use [assert]. You are welcome to find a shorter proof (but without using any more sophisticated tactics). *)
+
+(** Another question: Does [forall] also commute with [\/]? That is does 
+    [(forall x:A,P x \/ Q x) <-> (forall x:A, P x) \/ (forall x:A, Q x)]
+    hold? If not, how can you show that?
+*)
+
+(** * Existential quantification *)
+
+(** To say that there is an element of A having the property P, we write
+    [exists x:A, P x] more general we can form [exists x:A, PP]
+    where [PP] is a proposition possibly containing the variable [x]. 
+    Another example is [exists x:A,P x /\ Q x] meaning that there
+    is an element of [A] that has the property [P] and the
+    property [Q]. In our example that would mean that 
+    there is a student who is both clever and funny.
+
+    As an example we show that if there is an element of [A] having the
+    property [P] and that if whenever an element of [A] has the
+    property [P] has also the property [Q] then there is an elements of [A]
+    having the property [Q]. That is if there is a clever student, and every 
+    clever student is funny, then there is a funny student. In predicate
+    logic we write [(exists x:A,P x) -> (forall x:A,P x -> Q x) -> exists x:A, P x].
+
+    Btw, we are not changing the 2nd quantifier, it stays [forall]. What would happen
+    if we would replace it by [exists]
+
+    The syntactic conventions for [exists] are the same as for [forall]: the scope of an [exists]
+    always goes as far as possible. That is we read [exists x:A,P x /\ Q]
+    as [exists x:A, (P x /\ Q)]. 
+
+    The tactics for existential quatification are similar to the ones for conjunction. 
+    To prove an existential statement [exists x:A,PP] we use [exists a] where
+    [a : A] is our _witness_. We then have to prove [PP] where each free occurence of [x]
+    is replaced by [a]. To use an assumption [H : exists x:A,PP] we employ 
+    [destruct H as [a p]] which destructs [H] into [a : A] and [p : PP'] where [PP'] is [PP] 
+    where all free occurences of [x] have been replaced by [a].
+*)
+
+Lemma ExistsMono : (exists x:A,P x) -> (forall x:A,P x -> Q x) -> exists x:A, Q x.
+intros H1 H2.
+(** We first eliminate or assumption. *)
+destruct H1 as [a p].
+(** And now we introduce the existential. *)
+exists a.
+apply H2.
+(** In the last step we instantiated a universal quantifier. *)
+exact p.
+Qed.
+
+
+
+
+
+
+
+
 
 
 
